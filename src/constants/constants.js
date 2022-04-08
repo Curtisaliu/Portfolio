77a9516722c1a89e008dcd3a782a044a0c362296
Curtis A. Liu <Curtisaliu@gmail.com>
2022-04-07 22:06:35 -0700

    portfolio

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Place Holder',
+    description: "Filler.",
+      // image: '/images/1.png',
+      tags: ['Filler'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Place Holder',
+    description:"Filler.",
+    // image: '/images/2.png',
+    tags: ['Filler'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Place Holder',
+    description: "Filler.",
+      // image: '/images/3.jpg',
+      tags: ['Filler'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Placeholder',
+    description: "Filler.",
+    // image: '/images/4.jpg',
+    tags: ['Filler'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2016, text: 'Started College', },
+  { year: 2020, text: 'Finished College and started Law School', },
+  { year: 2021, text: 'Dropped out of Law School', },
+  { year: 2022, text: 'Started my journey with LEARN Academy', },
+  // { year: 2021, text: 'Started my own platform', },
+];
